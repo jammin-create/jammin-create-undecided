468b364a3523f044f76c81e822f754e9bf07f436
HeyImStas <beebx4f@protonmail.com>
2026-01-14 18:06:16 +0100

    fix log message

--- services/example-ajanta/main.py
+++ services/example-ajanta/main.py
@@ -1,5 +1,5 @@
 from aj_lang import service, refine, accumulate, on_transfer
-from aj_lang.host import LOG_INFO, get_service_info
+from aj_lang.host import LOG_DEBUG_INT, LOG_DEBUG_STR, get_service_info
 
 @service
 class MyService:
@@ -8,13 +8,14 @@ class MyService:
     @refine
     def refine(self, item_index: int, service_id: int, payload: bytes, payload_len: int, work_package_hash: bytes) -> bytes:
         """Refine handler."""
-        LOG_INFO("Example Service Refine, " + service_id)
+        LOG_DEBUG_INT("Example Service Refine", service_id)
         return b"Ok";
 
     @accumulate
-    def accumulate(self, slot: int, service_id: int, n_items: int) -> None:
+    def accumulate(self, timeslot: int, service_id: int, num_items: int) -> None:
         """Accumulation handler."""
-        LOG_INFO("Example Service Accumulate " + service_id + ", " + get_service_info().balance)
+        LOG_DEBUG_INT("Example Service Accumulate", service_id)
+        LOG_DEBUG_INT("Balance", get_service_info().balance)
         pass
 
     @on_transfer
@@ -24,5 +25,7 @@ class MyService:
             memo_str = memo.decode("utf-8")
         except UnicodeDecodeError:
             memo_str = "???"
-        LOG_INFO("Received transfer from " + sender + " of " + amount + " with memo: " + memo_str)
+        LOG_DEBUG_INT("Received transfer from", sender)
+        LOG_DEBUG_INT("Amount", amount)
+        LOG_DEBUG_STR("Memo", memo_str)
         pass
